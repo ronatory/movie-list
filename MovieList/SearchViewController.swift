20c4d06acfd3aff16aca8d186e6b548a2b92c254
ronatory <ronatory@gmail.com>
2016-09-25 18:44:13 +0200

    Add tab bar controller with two view controllers

--- MovieList/SearchViewController.swift
+++ MovieList/SearchViewController.swift
@@ -1,25 +1,24 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  MovieList
 //
-//  Created by Ronny Glotzbach on 24.09.16.
+//  Created by Ronny Glotzbach on 25.09.16.
 //  Copyright © 2016 Ronny Glotzbach. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class SearchViewController: UIViewController {
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
-
+	
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
-
+	
+	
 }
-
