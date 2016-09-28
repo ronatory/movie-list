841f2c809cf1b62a07f1a86e11472a06f92f17cf
ronatory <ronatory@gmail.com>
2016-09-28 20:50:18 +0200

    Add nothing found cell and refactoring

---
+++ MovieList/TableViewCellIdentifiers.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewCellIdentifiers.swift
+//  MovieList
+//
+//  Created by Ronny Glotzbach on 28.09.16.
+//  Copyright © 2016 Ronny Glotzbach. All rights reserved.
+//
+
+struct TableViewCellIdentifiers {
+	static let moviePopularCell = "MoviePopularCell"
+	static let movieSearchCell  = "MovieSearchCell"
+	static let nothingFoundCell = "NothingFoundCell"
+}
